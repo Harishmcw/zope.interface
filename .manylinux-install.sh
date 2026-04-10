4ebf53aaf60c6b0d11c31779b6e6170e9ba9eab2
Harish Raja Selvan <harish.rajaselvan@multicorewareinc.com>
2026-04-10 01:32:40 -0700

    Configuring for c-code

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Generated with zope.meta (https://zopemeta.readthedocs.io/) from:
-# https://github.com/zopefoundation/meta/tree/master/src/zope/meta/c-code
+# Generated from:
+# https://github.com/zopefoundation/meta/tree/master/config/c-code
 
 set -e -x
 
